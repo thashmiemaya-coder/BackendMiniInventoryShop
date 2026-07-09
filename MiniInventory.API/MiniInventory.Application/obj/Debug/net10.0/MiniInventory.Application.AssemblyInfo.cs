ab26312631954a1812460ea7c59d436a352b2cf9
Nathashaperera42 <shashininathasha42@gmail.com>
2026-07-09 14:57:24 +0530

    Update assembly informational version and cache files across multiple projects

--- MiniInventory.API/MiniInventory.Application/obj/Debug/net10.0/MiniInventory.Application.AssemblyInfo.cs
+++ MiniInventory.API/MiniInventory.Application/obj/Debug/net10.0/MiniInventory.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniInventory.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21325627e06203431703464b6b51f44988c7eb82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744fe420cd6fca13f244bc19b5f74065744484f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniInventory.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniInventory.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
